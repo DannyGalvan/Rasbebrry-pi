435c0458868a1cd3cc8d80b1266e9ffa2bd05407
DannyGalvan <cgalvan29111999@gmail.com>
2025-03-29 10:16:29 -0600

    cambio de sensor por potenciometro

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,28 @@
 
 #define NTC_PIN 26
 #define BETA 3950
-#define RESOLUTION 4095.0
+#define RESOLUTION 2047.0
 
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
 
 void setup()
 {
-  Serial.begin(9600);
   lcd.begin(16, 2);
 
-  analogReadResolution(12); // resolucion 12-bit mapping de 0-4095
+  analogReadResolution(11); // resolucion 11-bit mapping de 0 a 2047
 }
 
 void loop()
 {
-  int ntcValue = analogRead(NTC_PIN);
-  float voltage = ntcValue * (3.3 / RESOLUTION);
-  float temperature = 1 / (log(1 / (RESOLUTION / ntcValue - 1)) / BETA + 1.0 / 298.15) - 273.15;
+  int potenciometro = analogRead(NTC_PIN);
+  float voltage = potenciometro * (3.3 / RESOLUTION);
 
   lcd.setCursor(0, 0);
   lcd.print("Voltaje: ");
   lcd.print(voltage);
-
   lcd.setCursor(0, 1);
-  lcd.print("Temp: ");
-  lcd.print(temperature);
-  lcd.print(" C");
+  lcd.print("Pot: ");
+  lcd.print(potenciometro);
 
   delay(500);
 }
